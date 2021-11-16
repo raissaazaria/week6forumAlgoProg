c197e7406146b6791274aa165d0901e907d8aec6
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-11-16 09:47:14 +0700

    Update No 4 forum files.py

--- No 4 forum files.py
+++ No 4 forum files.py
@@ -3,11 +3,11 @@ import re
 os.getcwd()
 os.chdir('C:\\Users\\ASUS\\PycharmProjects\\pythonProject3')
 
-openText= open("mrMiyagi.txt","r")
+openText= open("mrMiyagi.txt","r") #open the txt and read
 readText= openText.read()
 openText.close()
 
-upperCase="([A-Z])"
+upperCase="([A-Z])" # from line 10-16 is the variabel that will appear in the text
 lowerCase="([a-z])"
 sampleTitles="(Mr|St|Mrs|Ms|Dr)[.]"
 periodsInternal = "[.](com|net|org|io|gov)"
@@ -15,13 +15,15 @@ suffixes = "(Inc|Ltd|Jr|Sr|Co)"
 other = "(i)[.](e)[.]"
 digits="([0-9])"
 
-editText= readText.replace("\n"," ")
-editText= re.sub(sampleTitles,"\\1<prd>",editText)
+editText= readText.replace("\n"," ") #replace line with space
+#avoids splitting in each grouping
+editText= re.sub(sampleTitles,"\\1<prd>",editText) 
 editText= re.sub(periodsInternal,"<prd>\\1",editText)
 editText= re.sub(""+suffixes+"[.]","\\1",editText)
 editText= re.sub("[.]"+digits,"<prd>\\1",editText)
 editText= re.sub(other,"i<prd>e<prd>",editText)
 
+#add stop and replace it with certain symbols
 editText= editText.replace("...","<prd><prd><prd><stop>")
 editText= editText.replace(".",".<stop>")
 editText= editText.replace("?","?<stop>")
