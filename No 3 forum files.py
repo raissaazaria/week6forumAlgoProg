2c0e92af512bff94f77fc62d83d5d3e7351dadae
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2021-11-16 09:40:34 +0700

    Update No 3 forum files.py

--- No 3 forum files.py
+++ No 3 forum files.py
@@ -1,10 +1,10 @@
-file= open("plainText.txt","r")
+file= open("plainText.txt","r") #open the text and read
 content = file.read()
-splitWords=content.split()
+splitWords=content.split() #split the word in content file
 
-wordTokenLength = sum(len(word)for word in content.split())
-wordToken =len(content.split())
-wordLength = wordTokenLength/wordToken
+wordTokenLength = sum(len(word)for word in content.split()) #sum the length of the word lin content.split
+wordToken =len(content.split()) #basically it just couunt the length in content.spliy
+wordLength = wordTokenLength/wordToken #the formula of the word length is the total of wordTokenLength divided with wordToken
 
-print("The average word length of the text is" ,round(wordLength,2))
+print("The average word length of the text is" ,round(wordLength,2)) #print the word and enter the wordLength with the rounding so it will not be full
 file.close()
